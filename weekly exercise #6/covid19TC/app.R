ec3b5f9b011bd5c21b4fd92c4d9eab97fc601430
tch8 <talia@chait.net>
2021-06-27 15:12:46 -0400

    continue working

--- weekly exercise #6/covid19TC/app.R
+++ weekly exercise #6/covid19TC/app.R
@@ -4,39 +4,47 @@ library(tidyverse)
 
 covid19 <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv")
 
-ui <- fluidPage(
-    selectInput(inputId = "state 1", 
+state_options <- covid19 %>% 
+  distinct(state) %>% 
+  pull(state)
+
+ui <- fluidPage( "COVID19 Cases App",
+    selectInput(inputId = "state1", 
                 label = "State 1:", 
-                choices = covid19,
+                choices = state_options,
                 selected = "Minnesota"),
-    selectInput(inputId = "state 2", 
+    selectInput(inputId = "state2", 
                 label = "State 2:", 
-                choices = covid19,
+                choices = state_options,
                 selected = "Massachusetts"),
-    selectInput(inputId = "state 3", 
+    selectInput(inputId = "state3", 
                 label = "State 3:", 
-                choices = covid19,
+                choices = state_options,
                 selected = "California"),
-    sliderInput(inputId = "Season", 
-                label = "Seasons:",
-                min = 2012, 
-                max = 2019, 
-                value = c(2013,2019),
-                sep = ""),
     submitButton(text = "Create plot"),
     plotOutput(outputId = "timeplot")
 )
 
 server <- function(input, output) {
-   # output$timeplot <- renderPlot({
-     #   babynames %>% 
-       #     filter(name == input$name, 
-      #             sex == input$sex) %>% 
-       #     ggplot() +
-       #     geom_line(aes(x = year, y = n)) +
-        #    scale_x_continuous(limits = input$years) +
-         #   theme_minimal()
- #   })
+    output$timeplot <- renderPlot({
+      covid19 %>% 
+        filter(cases >= 20,
+               state == c(input$state1, 
+                         input$state2, 
+                         input$state3)) %>%
+        ggplot(aes(x = date,
+                   y = cases,
+                   color = state)) +
+        geom_line() +
+        scale_y_log10() +
+        labs(x = "",
+             y = "",
+             title = "Log of Cumulative Cases Per State over Time",
+             caption = "Data from COVID 19 NY Times") +
+        theme(legend.position = "none",
+              plot.title.position = "plot",
+              plot.caption.position = "plot")
+    })
 }
 
 shinyApp(ui = ui, server = server)
